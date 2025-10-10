6c20f90e2e81483de52dcf5c92b8031be82c12fe
Pete Jelliffe <pete.jelliffe@gmail.com>
2025-10-09 22:47:55 -0400

    renamed file

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  base: "/acquire-startups-m1}",
+  base: "/acquire-startups-m1",
 });
